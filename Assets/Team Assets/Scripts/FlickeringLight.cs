20cb4f59e0f5482f5e66207a83a346e3aeb7b2a5
Kyle Grenier <kyle.grenier11@gmail.com>
2021-03-30 19:20:10 -0500

    Lights flicker at random times

--- Assets/Team Assets/Scripts/FlickeringLight.cs
+++ Assets/Team Assets/Scripts/FlickeringLight.cs
@@ -14,9 +14,18 @@ public class FlickeringLight : MonoBehaviour
     [SerializeField]
     private AudioClip[] flickeringSounds;
 
+    private Animator anim;
+
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        float time = Random.Range(0, 1f);
+        anim.Play("Chapel_Spotlight", 0, time);
     }
 
     public void FlickerLight()
